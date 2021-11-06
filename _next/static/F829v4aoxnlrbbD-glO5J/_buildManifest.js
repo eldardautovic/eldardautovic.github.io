a901fc5cb9b408c87051b031e66d1ab59ca0b469
eldardautovic <49655828+eldardautovic@users.noreply.github.com>
2021-11-06 15:44:13 +0000

    Deploying to gh-pages from @ eldardautovic/eldardautovic.github.io@e4f255742be8de28a8a4ed990113cb93e9d39bc0 🚀

--- _next/static/F829v4aoxnlrbbD-glO5J/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-ece63e87b990df8a.js","static/css/5859d8f3aed4dae2.css","static/chunks/pages/index-5f7714c372295963.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
