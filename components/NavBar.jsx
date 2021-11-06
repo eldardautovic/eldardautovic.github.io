38fb1b34c3cac3bb673987efe7a7d1015707a5fa
Eldar Dautovic <eldarcoox123@gmail.com>
2021-11-06 15:55:48 +0100

    Finished footer and everything related to clicking

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,18 +1,22 @@
-import style from '../styles/NavBar.module.css';
+import style from "../styles/NavBar.module.css";
 
 const Navbar = () => {
+  return (
+    <div className={style.container}>
+      <h1 className={style.logo}>ED</h1>
+      <ul className={style.list}>
+        <li className={style.listItem}>
+          <a href="#about">About me</a>
+        </li>
+        <li className={style.listItem}>
+          <a href="#mywork">My work</a>
+        </li>
+        <li className={style.listItem}>
+          <a href="mailto:dautoviceldar20@gmail.com">Contact me</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-
-    return (
-        <div className={style.container}>
-            <h1 className={style.logo}>ED</h1>
-            <ul className={style.list}>
-                <li className={style.listItem}><a href="">About me</a></li>
-                <li className={style.listItem}><a href="">My work</a></li>
-                <li className={style.listItem}><a href="">Contact me</a></li>
-            </ul>
-        </div>
-    )
-}
-
-export default Navbar
+export default Navbar;
